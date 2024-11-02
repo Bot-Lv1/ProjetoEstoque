814e4a87544b723b02fc41e298ecd8c5f9d0759e
Bot-Lv1 <133661511+Bot-Lv1@users.noreply.github.com>
2024-11-02 17:47:25 -0300

    BaseParametro

---
+++ ProjetoEstoque/src/dominio/BaseParametro.java
@@ -0,0 +1,45 @@
+package dominio;
+
+
+public abstract class BaseParametro{
+    protected int codigo;
+    protected String nome;
+    protected String descricao;
+    protected int cargaHoraria;
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public BaseParametro(){
+    }
+    public BaseParametro(int codigo, String nome, String descricao, int cargaHoraria){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+     
+
+}
